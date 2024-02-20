cb122f254525b120794fa6f4839406d5506d8837
Ross Bulat <ross@parity.io>
2024-02-20 11:48:26 +0700

    chore: fix license

--- src/docs/lib/Structure/Body/index.tsx
+++ src/docs/lib/Structure/Body/index.tsx
@@ -1,5 +1,5 @@
-// Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
-// SPDX-License-Identifier: GPL-3.0-only
+/* @license Copyright 2024 @polkadot-cloud/frontpage authors & contributors
+SPDX-License-Identifier: GPL-3.0-only */
 
 import { ComponentBase } from "@polkadot-cloud/react/types";
 
